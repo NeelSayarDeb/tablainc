d2c363a8359b7fb9c13f647315b55ae24eac9125
Neel <deb.neelsayar@gmail.com>
2022-08-29 23:12:45 +0530

    Routing added

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import $ from 'jquery';
 import Popper from 'popper.js';
 import './index.css';
 import App from './App';
+import Concept from './pages/Concept';
+import Navbar from './components/Navbar'
+import Footer from './components/Footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="concept" element={<Concept />} />
+      </Routes>
+    <Footer />
+  </BrowserRouter>
   </React.StrictMode>
 );
 
