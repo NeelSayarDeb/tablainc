a34cfa53489c7862e499d0185436f54fa18dd6ec
Neel <deb.neelsayar@gmail.com>
2022-08-29 23:15:05 +0530

    indentation fix

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ root.render(
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="concept" element={<Concept />} />
-      </Routes>
+    </Routes>
     <Footer />
   </BrowserRouter>
   </React.StrictMode>
